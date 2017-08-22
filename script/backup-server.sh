f1f27f68e36cc76c55371ad6eeff12fde121358f
Omniscimus <omniscimus@icloud.com>
2017-08-22 22:30:51 +0200

    Do not backup MySQL
    We're not using MySQL anymore.

--- script/backup-server.sh
+++ script/backup-server.sh
@@ -1,9 +1,6 @@
-#!/bin/sh
-# Backs up the Minecraft and MySQL servers.
+#!/bin/bash
+# Backs up the Minecraft server.
 # Use the -q option to only output errors.
-
-mysql_user=unknown
-mysql_password=secret
 
 repo_dir="$(dirname "${BASH_SOURCE[0]}")/.."
 backup_dir=${repo_dir}/backup
@@ -18,8 +15,5 @@ fi
 
 $verbose && echo "Backing up the server files..."
 tar czf ${backup_dir}/backup-survival-${today}.tar.gz -C $repo_dir server
-
-$verbose && echo "Backing up the SQL database..."
-mysqldump -u $mysql-user -p$mysql-password --databases unknown_minecraft | gzip > ${backup_dir}/backup-database-${today}.sql.gz
 
 $verbose && echo "Done!"
--- script/remove-yesterdays-backup.sh
+++ script/remove-yesterdays-backup.sh
@@ -1,9 +1,8 @@
-#!/bin/sh
-# Removes yesterday's Minecraft and MySQL server backups
+#!/bin/bash
+# Removes yesterday's Minecraft server backup
 
 yesterday=$(date -d "1 day ago" +%Y-%m-%d)
 dir="$(dirname "${BASH_SOURCE[0]}")/../backup"
 
-rm ${dir}/backup-database-${yesterday}.sql.gz
 rm ${dir}/backup-survival-${yesterday}.tar.gz
 
