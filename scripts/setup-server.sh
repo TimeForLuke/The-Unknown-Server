8940eec5c4b352c5a2d577c6f53025472ed1e8a6
Omniscimus <omni@omniscimus.net>
2017-12-10 14:28:23 +0100

    Be slightly more verbose while building

--- scripts/setup-server.sh
+++ scripts/setup-server.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 if [ ! -e /data/spigot-1.12.1.jar ]; then
+  echo "Compiling Spigot...\n"
   /scripts/compile-spigot.sh "/data"
 fi
 
 if [ ! -e /data/plugins/NoCheatPlus.jar ]; then
+  echo "Compiling plugins...\n"
   /scripts/compile-plugins "/data/plugins"
 fi
 
+echo "Starting the server...\n"
 /scripts/auto-restart-server.sh
